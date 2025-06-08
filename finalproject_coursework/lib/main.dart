ae4bd17559fb4a9d08381855262f40d174bcb633
KylanLim <yiksenglim6@gmail.com>
2025-06-09 00:14:09 +0800

    Hide the upper right corner debug

--- finalproject_coursework/lib/main.dart
+++ finalproject_coursework/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,  // Hide upper right corner debug text
       title: 'Final Project',
       theme: ThemeData(
         // This is the theme of your application.
